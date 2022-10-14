5b9b8e1ba93bc49c8c941886b1f906c75428f72d
thomas-waite <thomas.waite4@gmail.com>
2022-10-14 22:53:56 +0100

    test: validate token

--- src/Auth.sol
+++ src/Auth.sol
@@ -8,6 +8,16 @@ contract Auth is AccessControlEnumerable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant GOVERN_ROLE = keccak256("GOVERN_ROLE");
 
+    modifier onlyMinter() {
+        require(isMinter(msg.sender), "NOT_MINTER");
+        _;
+    }
+
+    modifier onlyGovernor() {
+        require(isGovernor(msg.sender), "NOT_GOVERNOR");
+        _;
+    }
+
     constructor() {
         // Setup GOVERN_ROLE to self and grant to deployer
         _setupRole(GOVERN_ROLE, address(this));
@@ -17,11 +27,15 @@ contract Auth is AccessControlEnumerable {
         _setRoleAdmin(GOVERN_ROLE, GOVERN_ROLE);        
     }
 
-    function isMinter(address _minter) external view returns (bool) {
+    function isMinter(address _minter) public view returns (bool) {
         return hasRole(MINTER_ROLE, _minter);
     }
 
-    function isGovernor(address _governor) external view returns (bool) {
+    function isGovernor(address _governor) public view returns (bool) {
         return hasRole(GOVERN_ROLE, _governor);
+    }
+
+    function grantMinter(address _minter) external onlyGovernor() {
+        grantRole(MINTER_ROLE, _minter);
     }
 }
--- test/GBPX.t.sol
+++ test/GBPX.t.sol
@@ -2,16 +2,25 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "./libs/Test.sol";
+import {Vm} from "./libs/Vm.sol";
 import {Auth} from "../src/Auth.sol";
 import {GBPX} from "../src/GBPX.sol";
 
 contract GBPXTest is Test {
     GBPX public gbpx;
 
+    address public minter = address(1);
+    address public user = address(2);
+    address public user2 = address(3);
+
+    Vm public constant vm = Vm(HEVM_ADDRESS);
+
     function setUp() public {
         Auth auth = new Auth();
-
         gbpx = new GBPX(address(auth));
+
+        // Grant MINTER to address
+        auth.grantMinter(minter);
     }
 
     function testInitialState() public {
@@ -19,11 +28,25 @@ contract GBPXTest is Test {
         assertEq(gbpx.symbol(), "GBP-X");
     }
 
-    function testTransfer() public {
+    function testMint() public {
+        assertEq(gbpx.balanceOf(user), 0);
 
+        vm.prank(minter);
+        gbpx.mint(user, 5);
+
+        assertEq(gbpx.balanceOf(user), 5);
     }
 
-    function testMint() public {
-        
+    function testTransfer() public {
+        assertEq(gbpx.balanceOf(user), 0);
+
+        vm.prank(minter);
+        gbpx.mint(user, 10);
+
+        vm.prank(user);
+        gbpx.transfer(user2, 5);
+
+        assertEq(gbpx.balanceOf(user), 5);
+        assertEq(gbpx.balanceOf(user2), 5);
     }
 }
