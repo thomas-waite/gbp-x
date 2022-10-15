1664ede9e0973ca0ae290264a9dae4dfc3ccd176
thomas-waite <thomas.waite4@gmail.com>
2022-10-15 15:39:59 +0100

    feat: add chainlink oracle wrapper tests

---
+++ test/integration/ChainlinkOracleWrapper.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+import {Test} from "../libs/Test.sol";
+import {MainnetAddresses} from "../../src/constants/MainnetAddresses.sol";
+import {ChainlinkOracleWrapper} from "../../src/oracle/ChainlinkOracleWrapper.sol";
+
+contract ChainlinkOracleWrapperIntegrationTest is Test {
+
+    ChainlinkOracleWrapper public oracle;
+
+    function setUp() public {
+        oracle = new ChainlinkOracleWrapper(MainnetAddresses.CHAINLINK_GBP_USDC_FEED);
+    }
+
+    function testInitialState() public {
+        assertEq(oracle.decimals(), 8);
+        assertEq(address(oracle.chainlinkOracle()), MainnetAddresses.CHAINLINK_GBP_USDC_FEED);
+    }
+
+    /// @notice Validate a reasonable price can be fetched
+    function testGetPrice() public {
+        uint256 gbpPerUsdPrice = oracle.getPrice();
+        assertGt(gbpPerUsdPrice, 0);
+        assertGt(gbpPerUsdPrice, 1e8); // >$1
+        assertLt(gbpPerUsdPrice, 1e8 + 5e7); // <$1.5
+    }
+}
---
+++ test/unit/GBPX.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+import {Test} from "../libs/Test.sol";
+import {Vm} from "../libs/Vm.sol";
+import {Auth} from "../../src/auth/Auth.sol";
+import {GBPX} from "../../src/token/GBPX.sol";
+
+contract GBPXTest is Test {
+    GBPX public gbpx;
+
+    address public minter = address(1);
+    address public user = address(2);
+    address public user2 = address(3);
+
+    Vm public constant vm = Vm(HEVM_ADDRESS);
+
+    function setUp() public {
+        Auth auth = new Auth();
+        gbpx = new GBPX(address(auth));
+
+        // Grant MINTER to address
+        auth.grantMinter(minter);
+    }
+
+    function testInitialState() public {
+        assertEq(gbpx.name(), "British Pound - X");
+        assertEq(gbpx.symbol(), "GBP-X");
+    }
+
+    function testMint() public {
+        assertEq(gbpx.balanceOf(user), 0);
+
+        vm.prank(minter);
+        gbpx.mint(user, 5);
+
+        assertEq(gbpx.balanceOf(user), 5);
+    }
+
+    function testTransfer() public {
+        assertEq(gbpx.balanceOf(user), 0);
+
+        vm.prank(minter);
+        gbpx.mint(user, 10);
+
+        vm.prank(user);
+        gbpx.transfer(user2, 5);
+
+        assertEq(gbpx.balanceOf(user), 5);
+        assertEq(gbpx.balanceOf(user2), 5);
+    }
+}
